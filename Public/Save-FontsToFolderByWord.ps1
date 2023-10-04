fbf92a9b9635ce05a7053d3e5be4470ce5cc1c4f
fmotion1 <futuremotionrecovery@gmail.com>
2023-10-04 07:27:40 -0500

    Small Update

--- Public/Save-FontsToFolderByWord.ps1
+++ Public/Save-FontsToFolderByWord.ps1
@@ -1,4 +1,3 @@
-using namespace System.IO
 function Save-FontsToFolderByWord {
     [CmdletBinding()]
     param (
@@ -19,96 +18,109 @@ function Save-FontsToFolderByWord {
         & "D:\Dev\Python\00 VENV\FontTools\Scripts\Activate.ps1"
         $VersionScript = "$env:FONTSCRIPTS\get_font_version.py"
 
-        $List = @()
+        $List = [System.Collections.Generic.List[String]]::new()
     }
 
     process {
-        foreach ($P in $Files) {
-            if     ($P -is [String]) { $List += $P }
-            elseif ($P.Path)         { $List += $P.Path }
-            elseif ($P.FullName)     { $List += $P.FullName }
-            elseif ($P.PSPath)       { $List += $P.PSPath }
-            else                     { Write-Warning "$P is an unsupported type." }
+
+        throw
+
+        try {
+            foreach ($P in $Files) {
+                if	 ($P -is [String]) { $List.Add($P) }
+                elseif ($P.Path)       { $List.Add($P.Path) }
+                elseif ($P.FullName)   { $List.Add($P.FullName) }
+                elseif ($P.PSPath)     { $List.Add($P.PSPath) }
+                else { Write-Error "Invalid argument passed to files parameter." }
+            }
+        }
+        catch {
+            $PSCmdlet.ThrowTerminatingError($PSItem)
         }
     }
 
     end {
 
-        $List | ForEach-Object -Parallel {
+        try{
+            $List | ForEach-Object -Parallel {
 
-            $FontFile = $_
+                $FontFile = $_
 
-            $FontFileName      = [System.IO.Path]::GetFileName($FontFile)
-            $FontDirectory     = [System.IO.Directory]::GetParent($FontFile)
+                $FontFileName      = [System.IO.Path]::GetFileName($FontFile)
+                $FontDirectory     = [System.IO.Directory]::GetParent($FontFile)
 
-            $FontFileVersion = & python $Using:VersionScript $FontFile
+                $FontFileVersion = & python $Using:VersionScript $FontFile
 
-            if($Using:NumWords -eq 1){
-                $RegExWord = '^(\w+)\b'
-                $RegExReplace = '$1\$0'
-            }
+                if($Using:NumWords -eq 1){
+                    $RegExWord = '^(\w+)\b'
+                    $RegExReplace = '$1\$0'
+                }
 
-            if($Using:NumWords -eq 2){
-                $RegExWord = '^(\w+)[\s|\-](\w+)\b'
-                $RegExReplace = '$1 $2\$0'
-            }
+                if($Using:NumWords -eq 2){
+                    $RegExWord = '^(\w+)[\s|\-](\w+)\b'
+                    $RegExReplace = '$1 $2\$0'
+                }
 
-            if($Using:NumWords -eq 3){
-                $RegExWord = '^(\w+)[\s|\-](\w+)[\s|\-](\w+)\b'
-                $RegExReplace = '$1 $2 $3\$0'
-            }
+                if($Using:NumWords -eq 3){
+                    $RegExWord = '^(\w+)[\s|\-](\w+)[\s|\-](\w+)\b'
+                    $RegExReplace = '$1 $2 $3\$0'
+                }
 
 
-            # Insert the first word occuring in the filename
-            # as a prefixed subdirectory
-            $Step1 = $FontFileName
-            $Step2 = $Step1 -replace $RegExWord, $RegExReplace
+                # Insert the first word occuring in the filename
+                # as a prefixed subdirectory
+                $Step1 = $FontFileName
+                $Step2 = $Step1 -replace $RegExWord, $RegExReplace
 
-            # Remove everything after the first '\' Leaving
-            # Just the first word.
-            $parts = $Step2 -split '\\'
-            $Step3 = $parts[0]
+                # Remove everything after the first '\' Leaving
+                # Just the first word.
+                $parts = $Step2 -split '\\'
+                $Step3 = $parts[0]
 
-            # Whitespace Cleanup
-            $Step4 = $Step3 -replace '\s+', ' '
-            $Step4 = $Step4.Trim()
+                # Whitespace Cleanup
+                $Step4 = $Step3 -replace '\s+', ' '
+                $Step4 = $Step4.Trim()
 
-            # Camel Case Conversion
-            #$Step5 = $Step4 -csplit '(?=[A-Z])' -ne '' -join ' '
-            #$Step5 = $Step4 -csplit '([a-z])([A-Z]\S)' -join ' '
-            $Step5 = $Step4 -csplit '(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])', -join ' '
-            #$Step5 = $Step4
-            Write-Host "`$Step5:" $Step5 -ForegroundColor Green
+                # Camel Case Conversion
+                $Step5 = $Step4 -csplit '(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])', -join ' '
+                Write-Host "`$Step5:" $Step5 -ForegroundColor Green
 
-            # Edge Case, Rename "Screen Smart" fonts correctly.
-            $Step6 = $Step5 -replace ' S Sm', ' SSm'
+                # Edge Case, Rename "Screen Smart" fonts correctly.
+                $Step6 = $Step5 -replace ' S Sm', ' SSm'
 
-            $FontExt = [IO.Path]::GetExtension($FontFile)
-            if($FontExt -eq '.ttf'){
-                $NextPathPart = 'TT'
-            }elseif($FontExt -eq '.otf'){
-                $NextPathPart = 'OT'
-            }else{
-                if ( $FontExt -match ".eot" -or
-                     $FontExt -match ".svg" -or
-                     $FontExt -match ".css" -or
-                     $FontExt -match ".woff" -or
-                     $FontExt -match ".woff2" -or
-                     $FontExt -match ".html" ){
-                        $NextPathPart = 'WEB'
-                     }
-            }
+                $FontExt = [IO.Path]::GetExtension($FontFile)
+                if($FontExt -eq '.ttf'){
+                    $NextPathPart = 'TT'
+                }elseif($FontExt -eq '.otf'){
+                    $NextPathPart = 'OT'
+                }else{
+                    if ( $FontExt -match ".eot" -or
+                         $FontExt -match ".svg" -or
+                         $FontExt -match ".css" -or
+                         $FontExt -match ".woff" -or
+                         $FontExt -match ".woff2" -or
+                         $FontExt -match ".html" ){
+                            $NextPathPart = 'WEB'
+                         }
+                }
 
-            $PathNoFn = "$FontDirectory\$Step6\$NextPathPart $FontFileVersion\"
-            if(!(Test-Path -LiteralPath $PathNoFn -PathType Container)){
-                #Write-Host "`$PathNoFn:" $PathNoFn -ForegroundColor Green
-                [IO.Directory]::CreateDirectory($PathNoFn) | Out-Null
-                # New-Item -Path $PathNoFn -ItemType Directory -Force
-            }
+                if([String]::IsNullOrWhiteSpace($FontFileVersion)){
+                    $PathNoFn = "$FontDirectory\$Step6\$NextPathPart\"
+                }else{
+                    $PathNoFn = "$FontDirectory\$Step6\$NextPathPart $FontFileVersion\"
+                }
 
-            $FinStep = [IO.Path]::Combine($PathNoFn, $FontFileName)
-            [IO.File]::Move($FontFile, $FinStep)
+                if(!(Test-Path -LiteralPath $PathNoFn -PathType Container)){
+                    [IO.Directory]::CreateDirectory($PathNoFn) | Out-Null
+                }
 
-        } -ThrottleLimit $MaxThreads
+                $FinStep = [IO.Path]::Combine($PathNoFn, $FontFileName)
+                [IO.File]::Move($FontFile, $FinStep)
+
+            } -ThrottleLimit $MaxThreads
+        }
+        catch {
+            $PSCmdlet.ThrowTerminatingError($PSItem)
+        }
     }
 }
